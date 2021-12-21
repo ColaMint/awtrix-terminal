c60198be2e131c735851084c3009d12fa157c7d9
Mint <463785757@qq.com>
2021-12-21 21:54:09 +0800

    doc

--- main.py
+++ main.py
@@ -13,17 +13,17 @@ if __name__ == '__main__':
                         dest='server_host',
                         type=str,
                         default='localhost',
-                        help='awtrix服务器域名')
+                        help='awtrix服务器域名（默认是为localhost）')
     parser.add_argument('--server-port',
                         dest='server_port',
                         type=int,
                         default=7001,
-                        help='awtrix服务器端口')
+                        help='awtrix服务器端口（默认为7001）')
     parser.add_argument('--pixel-size',
                         dest='pixel_size',
                         type=int,
                         default=2,
-                        help='awtrix像素尺寸')
+                        help='awtrix像素的尺寸（默认为2）')
     args = parser.parse_args()
 
     Awtrix(args.server_host, args.server_port, args.pixel_size)
